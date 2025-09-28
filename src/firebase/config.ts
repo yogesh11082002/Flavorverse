56f19abe7539342042a35991eef4d002337dff4f
Yogesh Thakur <yogeshthakur938920@gmail.com>
2025-09-28 06:57:46 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6837498735-36c41",
+  "appId": "1:530738767361:web:9eff4ed33ae2572f6c4200",
+  "apiKey": "AIzaSyCP0S2JtlAKhzJGYx1Ea_KMh9K9qQ7GWKc",
+  "authDomain": "studio-6837498735-36c41.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "530738767361"
+};
