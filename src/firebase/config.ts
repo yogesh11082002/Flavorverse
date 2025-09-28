b4cd490cf4cba39fdb7450f149a847ce8260497a
Yogesh Thakur <yogeshthakur938920@gmail.com>
2025-09-28 07:41:15 +0000

    apiKey: "AIzaSyAB9pD1KxKgHHtMGk_8KfL4uw8_TmrAhDc",   authDomain: "foodve

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "studio-6837498735-36c41",
-  "appId": "1:530738767361:web:9eff4ed33ae2572f6c4200",
-  "apiKey": "AIzaSyCP0S2JtlAKhzJGYx1Ea_KMh9K9qQ7GWKc",
-  "authDomain": "studio-6837498735-36c41.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "530738767361"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
